f4838f337cc89932a9f18d9ad541e48aec4bc286
Vagge <vaggeliskomnis45@gmail.com>
2024-01-08 13:44:04 +0200

    Repository Unit Testing Sample

--- app/src/test/java/gr/ihu/geoapp/ExampleUnitTest.java
+++ app/src/test/java/gr/ihu/geoapp/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import gr.ihu.geoapp.managers.Repository;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -11,7 +13,22 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testCreateUser() {
+        Repository repository = new Repository();
+        String testEmail = "test@example.com";
+        String testPassword = "testpassword";
+
+        System.out.println(repository.createUser(testEmail, testPassword).getResult());
+        assertNotNull(repository.createUser(testEmail, testPassword).getResult());
+    }
+
+    @Test
+    public void testCheckUser() {
+        Repository repository = new Repository();
+        String testEmail = "test@example.com";
+        String testPassword = "testpassword";
+
+        System.out.println(repository.checkUser(testEmail, testPassword).getResult());
+        assertNotNull(repository.checkUser(testEmail, testPassword).getResult());
     }
 }
